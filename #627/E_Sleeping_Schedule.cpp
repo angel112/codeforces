20584ed1f8544ed0bade7813a5f7d684698379f4
angel112 <ngllakra@gmail.com>
2020-04-19 03:20:30 +0530

    sleeping schedule

--- #627/E_Sleeping_Schedule.cpp
+++ #627/E_Sleeping_Schedule.cpp
@@ -1,43 +1,43 @@
 #include <bits/stdc++.h>
 
-#define lli long long int
+#define ll long long int
 
 using namespace std;
 
-int main() {
-
-    lli n, h, l, r;
+int main()
+{
+    ios::sync_with_stdio(0);
+    int n, h, l, r;
+    int ans = 0;
     cin >> n >> h >> l >> r;
-    lli gd = 0, sc = 0, hour = 0;
-    for (lli i = 0 ; i < n ; i++) {
-        lli x;
-        cin >> x;
-        sc++;
-        hour += x;
-        
-        if (hour >= l && hour <= r) {
-            gd++;
-        }
-        else {
-            if (((hour - r) > 0) && ((hour - r) <= sc)) {
-                sc = sc - (hour - r);
-                hour = r;
-                gd++; 
-            }  else {
-                if (hour >= h)
-                    hour -= h;
-                if (hour >= l && hour <= r) 
-                    gd++;    
-                else if (((hour - r) > 0) && ((hour - r) <= sc)) {
-                    sc = sc - (hour - r);
-                    hour = r;
-                    gd++;
-                }
-            }         
-        }
-        if (hour >= h)
-            hour -= h;
+    vector<vector<int>> dp(n + 1, vector<int>(n + 1, INT_MIN));
+    vector<int> a(n + 1);
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> a[i];
+        if (i > 1)
+            a[i] += a[i - 1];
     }
-    cout << gd;
+
+    dp[0][0] = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            ll s = a[i + 1] - j;
+            dp[i + 1][j] = max(dp[i + 1][j], dp[i][j] + (((s % h) >= l && (s % h) <= r) ? 1 : 0));
+            s--;
+            dp[i + 1][j + 1] = max(dp[i + 1][j + 1], dp[i][j] + (((s % h) >= l && (s % h) <= r) ? 1 : 0));
+        }
+    }
+    for (int i = 0; i <= n; i++)
+    {
+        if (ans < dp[n][i])
+            ans = dp[n][i];
+    }
+
+    cout << ans;
+
     return 0;
 }
